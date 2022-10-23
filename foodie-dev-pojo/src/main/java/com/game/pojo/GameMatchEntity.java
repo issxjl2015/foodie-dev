a51c8ddc1b472aacaa3dee5930e9d996292571d7
issxjl2015 <issxjl2015@outlook.com>
2022-10-23 20:43:30 +0800

    game match表的entity

---
+++ foodie-dev-pojo/src/main/java/com/game/pojo/GameMatchEntity.java
@@ -0,0 +1,179 @@
+package com.game.pojo;
+
+import java.util.Date;
+
+/**
+ * game_match_info表的entity
+ */
+public class GameMatchEntity {
+
+    private long id;
+
+    private String gameType;
+
+    private String matchName;
+
+    private String matchDesc;
+
+    private Date matchTime;
+
+    private String homeTeam;
+
+    private String visitingTeam;
+
+    private String gameScore;
+
+    private String gameResult;
+
+    private String winTeam;
+
+    private String language;
+
+    private int enabledFlag;
+
+    private int deletedFlag;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String updateBy;
+
+    private Date updateTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+
+    public String getMatchName() {
+        return matchName;
+    }
+
+    public void setMatchName(String matchName) {
+        this.matchName = matchName;
+    }
+
+    public String getMatchDesc() {
+        return matchDesc;
+    }
+
+    public void setMatchDesc(String matchDesc) {
+        this.matchDesc = matchDesc;
+    }
+
+    public Date getMatchTime() {
+        return matchTime;
+    }
+
+    public void setMatchTime(Date matchTime) {
+        this.matchTime = matchTime;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getVisitingTeam() {
+        return visitingTeam;
+    }
+
+    public void setVisitingTeam(String visitingTeam) {
+        this.visitingTeam = visitingTeam;
+    }
+
+    public String getGameScore() {
+        return gameScore;
+    }
+
+    public void setGameScore(String gameScore) {
+        this.gameScore = gameScore;
+    }
+
+    public String getGameResult() {
+        return gameResult;
+    }
+
+    public void setGameResult(String gameResult) {
+        this.gameResult = gameResult;
+    }
+
+    public String getWinTeam() {
+        return winTeam;
+    }
+
+    public void setWinTeam(String winTeam) {
+        this.winTeam = winTeam;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getEnabledFlag() {
+        return enabledFlag;
+    }
+
+    public void setEnabledFlag(int enabledFlag) {
+        this.enabledFlag = enabledFlag;
+    }
+
+    public int getDeletedFlag() {
+        return deletedFlag;
+    }
+
+    public void setDeletedFlag(int deletedFlag) {
+        this.deletedFlag = deletedFlag;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
